b20d0190c48d1c84fdc18febb4d532e3b312b573
Volta <giovoltadesign@gmail.com>
2021-03-28 13:07:25 -0300

    render character destructuring assignment

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,24 +1,32 @@
-const Character = () => {
-    // vista de la descripción de cada uno de los personajes
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+
+
+const Character = async () => {
+    //vista descrición personajes
+    const id = getHash();
+    const character = await getData(id);
+  
+    const { image, name,  episode, status, species, gender, origin, location} = character;
+  
     const view = `
-        <div class="Character-inner">
-            <article class="character-card">
-                <img src="" alt="name">
-                <h2>Name</h2>
-            </article>
-            <article class="character-card">
-                <h3>Episodes: </h3>
-                <h3>Status: </h3>
-                <h3>Species: </h3>
-                <h3>Gender: </h3>
-                <h3>Origin: </h3>
-                <h3>Last location: </h3>
-            </article>
-        </div>
-    
+      <div class="Characters-inner">
+        <article class="Characters-card">
+          <img src="${image}" alt="${name}">
+          <h2>${name}</h2>
+        </article>
+        <article class="Character-card">
+          <h3>span Episodes: <span>${episode.length}</span></h3>
+          <h3>Status: <span>${status}</span></h3>
+          <h3>Species: <span>${species}</span></h3>
+          <h3>Gender: <span>${gender}</span></h3>
+          <h3>Origin: <span>${origin.name}</span></h3>
+          <h3>Last Location: <span>${location.name}</span></h3>
+        </article
+      </div>
     `;
-
     return view;
-}
+  };
 
-export default Character;
+  export default Character;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,13 +1,14 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = async (id) => {
-  const apiUrl = id ? `${API}${id}` : API;
-  try {
-    const response = fetch(apiUrl);
-    const data = await response.json();//convierte a json
-  } catch(error) {
-    console.log('Fetch error!', error)
-  }
-}
+    const apiUrl = id ? `${API}${id}` : API;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();//convierte el text a json devuleve una promises
+        return data;
+    } catch (error) {
+        console.log('Fetch Error', error);
+    };
+};
 
 export default getData;
